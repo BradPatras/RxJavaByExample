923e042c4bea2b137d91062cb23cad08aa8c2437
Mike Casper <Mikecasper395@gmail.com>
2019-01-08 20:45:41 -0600

    Add possible solutions

--- app/src/main/java/io/github/mcasper3/rxjavabyexample/example1/Example1.kt
+++ app/src/main/java/io/github/mcasper3/rxjavabyexample/example1/Example1.kt
@@ -1,14 +1,54 @@
 package io.github.mcasper3.rxjavabyexample.example1
 
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
 
 class Example1(private val dataSource: DataSource) {
 
-    fun getListOfUsers(): Observable<List<UserUiModel>> = TODO()
+    fun getListOfUsers(): Observable<List<UserUiModel>> = dataSource.getUsers()
+        .map { users -> users.map { user -> mapUser(user) } }
 
-    fun getListOfUsersInCity(city: String): Observable<List<UserUiModel>> = TODO()
+    fun getListOfUsersInCity(city: String): Observable<List<UserUiModel>> = dataSource.getUsers()
+        .flatMapIterable { it }
+        .filter { it.location.city == city }
+        .map { mapUser(it) }
+        .toList()
+        .toObservable()
 
-    fun getListOfUsersInStateOrderedByLastName(state: String): Observable<List<UserUiModel>> = TODO()
+    fun getListOfUsersInStateOrderedByLastName(state: String): Observable<List<UserUiModel>> = dataSource.getUsers()
+        .flatMapIterable { it }
+        .filter { it.location.state == state }
+        .sorted { user1, user2 -> user1.lastName.compareTo(user2.lastName) }
+        .map { mapUser(it) }
+        .toList()
+        .toObservable()
 
-    fun getListOfUsersAndCompaniesInState(state: String): Observable<Pair<List<UserUiModel>, List<CompanyUiModel>>> = TODO()
+    fun getListOfUsersAndCompaniesInState(state: String): Observable<Pair<List<UserUiModel>, List<CompanyUiModel>>> {
+        val usersSingle = dataSource.getUsers()
+            .flatMapIterable { it }
+            .filter { it.location.state == state }
+            .map { mapUser(it) }
+            .toList()
+        val companiesSingle = dataSource.getCompanies()
+            .flatMapIterable { it }
+            .filter { it.location.state == state }
+            .map { company -> CompanyUiModel(company.name, mapLocation(company.location), company.phoneNumber) }
+            .toList()
+        return Single.zip(
+            usersSingle,
+            companiesSingle,
+            BiFunction { users: List<UserUiModel>, companies: List<CompanyUiModel> -> users to companies }
+        ).toObservable()
+    }
+
+    private fun mapLocation(location: LocationDbModel): String {
+        return with(location) { "$city, $state, $country" }
+    }
+
+    private fun mapUser(user: UserDbModel): UserUiModel {
+        val address = mapLocation(user.location)
+        val initials = "${user.firstName.first()}${user.lastName.first()}"
+        return UserUiModel("${user.firstName} ${user.lastName}", initials, address, user.phoneNumber, user.email)
+    }
 }
--- app/src/main/java/io/github/mcasper3/rxjavabyexample/example2/Example2.kt
+++ app/src/main/java/io/github/mcasper3/rxjavabyexample/example2/Example2.kt
@@ -2,11 +2,17 @@ package io.github.mcasper3.rxjavabyexample.example2
 
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import java.util.concurrent.TimeUnit
 
 class Example2(private val dataSource: DataSource) {
 
-    fun getAggregateMaxAgeOfUserGroups(userGroupIds: List<Long>): Observable<Int> = TODO()
+    fun getAggregateMaxAgeOfUserGroups(userGroupIds: List<Long>): Observable<Int> = Observable.fromIterable(userGroupIds)
+        .flatMap { dataSource.getUsersInGroup(it).map { users -> users.maxBy { user -> user.age }?.age ?: 0 } }
+        .reduce { total: Int, nextAge: Int -> total + nextAge }
+        .toObservable()
 
     // Every 5 seconds, get the next 5 numbers and add them to the previous result and output that result
-    fun getAggregate(intervalScheduler: Scheduler): Observable<Int> = TODO()
+    fun getAggregate(intervalScheduler: Scheduler): Observable<Int> = Observable.interval(5, TimeUnit.SECONDS, intervalScheduler)
+        .map { dataSource.getNextFiveNumbers().sum() }
+        .scan { previousTotal, newValue -> previousTotal + newValue }
 }
